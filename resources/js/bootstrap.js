4353f19ab7633c3f5f0205beb279cf59f0d48ba8
Francisco Cavasan <francisco.cavasan12@gmail.com>
2024-06-25 01:47:31 -0300

    feat: add access control origin headers

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -8,6 +8,8 @@ import axios from 'axios';
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+// set allow headers for all requests
+window.axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
